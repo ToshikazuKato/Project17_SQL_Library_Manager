fa22f9541c8f640596763bf05a1e746c890ee509
Toshikazu Horioka <toshikazu@evolany.com>
2019-04-30 16:26:07 +0900

    display book detail, update features

--- routes/books.js
+++ routes/books.js
@@ -2,24 +2,22 @@ const Book = require('../models').Book;
 const express = require('express');
 const router = express.Router();
 
+// display all books
 router.get('/', (rq, rs) => {
 	Book.findAll().then(books => {
-			console.log(books);
 			rs.render('index', { books: books });
 		}).catch(err => {
 			rs.send(500, err);
 		});
 });
 
+// display book creation form
 router.get('/new',(rq,rs)=>{
-	rs.render('new-book');
+	rs.render('new_book');
 });
 
+// insert a book into table
 router.post('/new',(rq,rs)=>{
-	// var title = rq.body.title;
-	// var author = rq.body.author;
-	// var genre = rq.body.genre;
-	// var year = rq.body.year;
 	 let book = {
 		 'title' : rq.body.title,
 		 'author' : rq.body.author,
@@ -30,7 +28,6 @@ router.post('/new',(rq,rs)=>{
 	Book.findOne({where:book}).then(bk=>{
 		if(bk){
 			//display errors that book is already in the table
-			//rs.render("new-book", { alert: "This book already exists in the shelf" });
 		}else{
 			// create a new book
 			Book.create(book).then(newBook => {
@@ -41,8 +38,31 @@ router.post('/new',(rq,rs)=>{
 			});
 		}
 	});
-
-
 });
+
+// display book detail
+router.get('/:id',(rq,rs)=>{
+	Book.findByPk(rq.params.id).then(book => {
+		rs.render('book_detail',{book:book});
+	}).catch(err=>{
+		console.log(err);
+	});
+});
+
+//update book
+router.post('/:id',(rq,rs)=>{
+	Book.findByPk(rq.params.id).then(book => {
+		if(book){
+			console.log(rq.body);
+			return book.update(rq.body);
+		}else{
+			//err
+		}
+	}).then(()=>{
+		rs.redirect('/books');
+	});
+})
+
+
 
 module.exports = router;

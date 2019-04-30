d8db1c8242d01cab4209d62f1a8eeb2ec0c6a938
Toshikazu Horioka <toshikazu@evolany.com>
2019-04-30 11:59:58 +0900

    book create feature, but no validation yet

--- routes/books.js
+++ routes/books.js
@@ -15,4 +15,34 @@ router.get('/new',(rq,rs)=>{
 	rs.render('new-book');
 });
 
+router.post('/new',(rq,rs)=>{
+	// var title = rq.body.title;
+	// var author = rq.body.author;
+	// var genre = rq.body.genre;
+	// var year = rq.body.year;
+	 let book = {
+		 'title' : rq.body.title,
+		 'author' : rq.body.author,
+		 'genre' : rq.body.genre,
+		 'year' : rq.body.year,
+	 };
+	// check if the book is already in the table
+	Book.findOne({where:book}).then(bk=>{
+		if(bk){
+			//display errors that book is already in the table
+			//rs.render("new-book", { alert: "This book already exists in the shelf" });
+		}else{
+			// create a new book
+			Book.create(book).then(newBook => {
+				console.log(newBook+' is successfully registered');
+				rs.redirect('/books');
+			}).catch(err=>{
+				console.log(err.errors);
+			});
+		}
+	});
+
+
+});
+
 module.exports = router;
